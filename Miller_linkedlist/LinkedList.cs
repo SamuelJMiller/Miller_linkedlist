368599543dca8a189332d64a5e3d8f29fb07ae8d
samuel miller <samuel.miller@student.centralia.edu>
2021-10-07 17:00:08 -0700

    LinkedList and Node classes

---
+++ Miller_linkedlist/LinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miller_linkedlist
+{
+    class LinkedList
+    {
+        private Node _head;
+
+        public Node Head
+        {
+            get { return _head; }
+            set { _head = value; }
+        }
+    }
+}
---
+++ Miller_linkedlist/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miller_linkedlist
+{
+    class Node
+    {
+        private string _data;
+        private Node _next;
+
+        public Node(string data)
+        {
+            _data = data;
+        }
+
+        public string Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public Node Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+    }
+}
